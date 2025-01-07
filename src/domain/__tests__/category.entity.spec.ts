e985f814fcc983e11b57a8e80a2bfc83d7d0f90f
21lucasmessias <21.lucasmessias@gmail.com>
2025-01-07 18:23:24 -0300

    test: enhance unit tests for Category entity with additional methods and assertions

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -27,7 +27,7 @@ describe('Category Entity', () => {
         expect(category.name).toBe(categoryCreateCommand.name);
         expect(category.description).toBe(categoryCreateCommand.description);
         expect(category.isActive).toBe(categoryCreateCommand.isActive);
-        expect(category.createdAt).toBeDefined();
+        expect(category.createdAt).toBeInstanceOf(Date);
         expect(category.categoryId).toBeUndefined();
       });
     });
@@ -63,6 +63,58 @@ describe('Category Entity', () => {
         expect(category.description).toBe(categoryConstructorProps.description);
         expect(category.isActive).toBe(categoryConstructorProps.isActive);
         expect(category.createdAt).toBe(categoryConstructorProps.createdAt);
+      });
+    });
+  });
+
+  describe('given a Category instance', () => {
+    let category: Category;
+
+    beforeEach(() => {
+      category = Category.create({
+        name: 'Category Name',
+        description: 'Category Description',
+        isActive: true,
+      });
+    });
+
+    describe('when changeName is called', () => {
+      beforeEach(() => {
+        category.changeName('New Category Name');
+      });
+
+      test('then should have the new name', () => {
+        expect(category.name).toBe('New Category Name');
+      });
+    });
+
+    describe('when changeDescription is called', () => {
+      beforeEach(() => {
+        category.changeDescription('New Category Description');
+      });
+
+      test('then should have the new description', () => {
+        expect(category.description).toBe('New Category Description');
+      });
+    });
+
+    describe('when activate is called', () => {
+      beforeEach(() => {
+        category.activate();
+      });
+
+      test('then should be active', () => {
+        expect(category.isActive).toBe(true);
+      });
+    });
+
+    describe('when deactivate is called', () => {
+      beforeEach(() => {
+        category.deactivate();
+      });
+
+      test('then should be inactive', () => {
+        expect(category.isActive).toBe(false);
       });
     });
   });
