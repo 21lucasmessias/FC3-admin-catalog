2c1258ddf74cdb82af7b0ea4ed8279a910761b6b
21lucasmessias <21.lucasmessias@gmail.com>
2025-02-02 19:19:26 -0300

    feat: implement CategoryInMemoryRepository to use SearchableRepository

--- src/category/infra/db/category-in-memory.repository.ts
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -1,9 +1,30 @@
 import { Category } from 'src/category/domain/category.entity';
+import { SortDirection } from 'src/shared/domain/repository/search-params';
 import { Uuid } from 'src/shared/domain/value-objects/uuid.vo';
-import { InMemoryRepository } from 'src/shared/infra/db/in-memory/in-memory.repository';
+import { InMemorySearchableRepository } from 'src/shared/infra/db/in-memory/in-memory-searchable.repository';
 
-export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid> {
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> {
+  sortableFields: string[] = ['createdAt', 'name'];
+
+  protected async applyFilter(entities: Map<string, Category>, filter: string | null): Promise<Category[]> {
+    const items = Array.from(entities.values());
+
+    if (!filter) return items;
+
+    return items.filter((category) => {
+      return category.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
   getEntity(): new (...args: any[]) => Category {
     return Category;
+  }
+
+  protected applySort(entities: Category[], sort: string | null, sortDir: SortDirection | null): Category[] {
+    if (sort) {
+      return super.applySort(entities, sort, sortDir);
+    }
+
+    return super.applySort(entities, 'createdAt', 'desc');
   }
 }
